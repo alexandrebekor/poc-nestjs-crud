9d09c47c305d9f5a991c412480a81d016350a172
Alexandre Bekor <contato@alexandrebekor.com>
2022-01-25 16:24:38 -0300

    added module product

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common'
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'I am FullStack!'
+    return 'I am FullStack Javascript!'
   }
 }
